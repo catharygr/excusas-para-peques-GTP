7d829880bb6b0d80256b41f0eacdb70b6962bb41
catharygr <103951447+catharygr@users.noreply.github.com>
2023-06-13 20:21:42 +0100

    Trabajando con el css y html

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
 import "./App.css";
 
-function App() {}
+function App() {
+  return (
+    <div>
+      <header>
+        <img src="./images/images.jpg" alt="Lapiz alegre" />
+        <h1>Excusas para peques</h1>
+      </header>
+      <section id="setup-container">
+        <div className="setup-inner">
+          <div className="speech-bubble-ai" id="speech-bubble-ai">
+            <p id="movie-boss-text">
+              Give me a one-sentence concept and I'll give you an eye-catching
+              title, a synopsis the studios will love, a movie poster... AND
+              choose the cast!
+            </p>
+          </div>
+        </div>
+        <div
+          className="setup-inner setup-input-container"
+          id="setup-input-container"
+        >
+          <textarea
+            id="setup-textarea"
+            placeholder="An evil genius wants to take over the world using AI."
+          ></textarea>
+          <button className="send-btn" id="send-btn" aria-label="send"></button>
+        </div>
+      </section>
+    </div>
+  );
+}
 
 export default App;
