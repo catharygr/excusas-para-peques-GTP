49c2481d33008764748b22f72513ed786e83a91a
catharygr <103951447+catharygr@users.noreply.github.com>
2023-06-14 18:56:49 +0100

    Trabajando con openAIApi

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,72 @@
-import "./App.css";
+import  { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
 
 function App() {
+  const [excuse, setExcuse] = useState("");
+
+  const generateExcuse = async () => {
+    try {
+      const openaiConfig = new Configuration({
+        apiKey: "TU_API_KEY_DE_OPENAI", 
+      });
+
+      const openaiApi = new OpenAIApi(openaiConfig);
+      const prompt =
+        "Un genio maligno quiere conquistar el mundo utilizando la IA.";
+
+      const response = await openaiApi.completions.create({
+        engine: "davinci",  openai
+        prompt,
+        max_tokens: 100,
+      });
+
+      const excuseText = response.choices[0].text.trim();
+      setExcuse(excuseText);
+    } catch (error) {
+      console.error("Error al generar la excusa:", error);
+    }
+  };
+
   return (
     <div>
       <header>
         <img src="./images/images.jpg" alt="Lapiz alegre" />
         <h1>Excusas para peques</h1>
       </header>
-      <section id="setup-container">
-        <div className="setup-inner">
-          <div className="speech-bubble-ai" id="speech-bubble-ai">
-            <p id="excusas-boss-text">
-              Ups! Parece que se me olvidó hacer la tarea. Pero es que un
-              alienígena aterrizó en nuestro jardín y me distrajo toda la noche.
-              ¡Fue realmente increíble
-            </p>
+      <main>
+        <section id="setup-container">
+          <div className="setup-inner">
+            <img src="/images/ett.jpg" alt="Imagen" />
+            <div className="speech-bubble-ai" id="speech-bubble-ai">
+              <p id="excusas-boss-text">{excuse}</p>
+            </div>
           </div>
-        </div>
-        <div
-          className="setup-inner setup-input-container"
-          id="setup-input-container"
-        >
-          <textarea
-            id="setup-textarea"
-            placeholder="Un genio maligno quiere conquistar el mundo utilizando la IA."
-          ></textarea>
-          <button className="send-btn" id="send-btn" aria-label="send"></button>
-        </div>
-      </section>
+          <div
+            className="setup-inner setup-input-container"
+            id="setup-input-container"
+          >
+            <textarea
+              id="setup-textarea"
+              placeholder="Un genio maligno quiere conquistar el mundo utilizando la IA."
+            ></textarea>
+            <button
+              className="send-btn"
+              id="send-btn"
+              aria-label="send"
+              onClick={generateExcuse}
+            >
+              <img src="images/flecha.jpg" alt="flecha"></img>
+            </button>
+          </div>
+        </section>
+        <section className="output-container" id="output-container">
+          <div id="output-img-container" className="output-img-container"></div>
+          <h1 id="output-title"></h1>
+          <h2 id="output-stars"></h2>
+          <p id="output-text"></p>
+        </section>
+      </main>
+      <footer>&copy; 2023 ExcusasPeques todos los derechos reservados</footer>
     </div>
   );
 }
