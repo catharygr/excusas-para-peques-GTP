d3bf11105ba1ca3bb12f5d4cbb29213c8719593b
catharygr <103951447+catharygr@users.noreply.github.com>
2023-06-16 18:38:42 +0100

    trabajando con css y en la lógica

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,10 @@ function App() {
 
   async function generateExcuse() {
     try {
-      const question =
-        "Contestar a la pregunta: ¿Por qué no has hecho los deberes?";
       const response = await axios.post(
         "https://api.openai.com/v1/engines/text-davinci-003/completions",
         {
-          prompt: question,
+          prompt: "Contestar a la pregunta: ¿Por qué no has hecho los deberes?",
           max_tokens: 150,
           temperature: 0.9,
         },
@@ -56,7 +54,7 @@ function App() {
           >
             <textarea
               id="setup-textarea"
-              placeholder="Contestar a la pregunta: ¿Por qué no has hecho los deberes?"
+              placeholder="Un genio maligno quiere conquistar el mundo utilizando la IA."
               value={excuse}
               onChange={handleTextareaChange}
             ></textarea>
@@ -83,85 +81,3 @@ function App() {
 }
 
 export default App;
-
-// import { useState } from "react";
-// import axios from "axios";
-// import "./App.css";
-// import { process } from "./env";
-
-// function App() {
-//   const [excuse, setExcuse] = useState("");
-
-//   async function generateExcuse() {
-//     try {
-//       const question =
-//         "Contestar a la pregunta: ¿Por qué no has hecho los deberes?";
-//       const response = await axios.post(
-//         "https://api.openai.com/v1/engines/text-davinci-003/completions",
-//         {
-//           prompt: question,
-//           max_tokens: 150,
-//           temperature: 0.9,
-//         },
-//         {
-//           headers: {
-//             Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-//           },
-//         }
-//       );
-
-//       const excuseText = response.data.choices[0].text.trim();
-//       console.log("Excusa generada:", excuseText);
-//       setExcuse(excuseText);
-//     } catch (error) {
-//       console.error("Error al generar la excusa:", error);
-//     }
-//   }
-
-//   return (
-//     <div>
-//       <header>
-//         <img src="./images/images.jpg" alt="Lapiz alegre" />
-//         <h1>Excusas para peques</h1>
-//       </header>
-//       <main>
-//         <section id="setup-container">
-//           <div className="setup-inner">
-//             <img src="/images/ett.jpg" alt="Imagen" />
-//             <div className="speech-bubble-ai" id="speech-bubble-ai">
-//               <p id="excusas-boss-text">{excuse}</p>
-//             </div>
-//           </div>
-//           <div
-//             className="setup-inner setup-input-container"
-//             id="setup-input-container"
-//           >
-//             <textarea
-//               id="setup-textarea"
-//               placeholder="Contestar a la pregunta: ¿Por qué no has hecho los deberes?"
-//               value={excuse}
-//               readOnly
-//             ></textarea>
-//             <button
-//               className="send-btn"
-//               id="send-btn"
-//               aria-label="send"
-//               onClick={generateExcuse}
-//             >
-//               <img src="images/flecha.jpg" alt="flecha" />
-//             </button>
-//           </div>
-//         </section>
-//         <section className="output-container" id="output-container">
-//           <div id="output-img-container" className="output-img-container"></div>
-//           <h1 id="output-title"></h1>
-//           <h2 id="output-stars"></h2>
-//           <p id="output-text"></p>
-//         </section>
-//       </main>
-//       <footer>&copy; 2023 ExcusasPeques todos los derechos reservados</footer>
-//     </div>
-//   );
-// }
-
-// export default App;
