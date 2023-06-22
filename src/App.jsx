18640dd36f84656f82f48f7175435e9fab8d6a8c
catharygr <103951447+catharygr@users.noreply.github.com>
2023-06-22 17:10:45 +0100

    Trabajando con la respuesta del box

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Configuration, OpenAIApi } from "openai";
 import { process } from "./env";
@@ -12,19 +12,29 @@ const openai = new OpenAIApi(configuration);
 function App() {
   const [excuse, setExcuse] = useState("");
   const [response, setResponse] = useState("");
+  const [bossText, setBossText] = useState(
+    "¡Hola! Soy el jefe de ExcusasPeques. ¿Por qué no has hecho los deberes?"
+  );
 
   async function generateExcuse() {
     try {
+      setBossText("Espera en un momento, te responderé...");
+
       const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: "Contestar a la pregunta: ¿Por qué no has hecho los deberes?",
         max_tokens: 70,
         temperature: 0.9,
       });
+
       const excuseText = response.data.choices[0].text.trim();
       console.log("Excusa generada:", excuseText);
+
       setExcuse(""); // Limpiar el textarea
       setResponse(excuseText);
+      setBossText(
+        "¡Hola! Soy el jefe de ExcusasPeques. ¿Por qué no has hecho los deberes?"
+      );
     } catch (error) {
       console.error("Error al generar la excusa:", error);
     }
@@ -45,10 +55,7 @@ function App() {
           <div className="setup-inner">
             <img src="/images/ett.jpg" alt="Imagen" />
             <div className="speech-bubble-ai" id="speech-bubble-ai">
-              <p id="excusas-boss-text">
-                ¡Hola! Soy el jefe de ExcusasPeques. ¿Por qué no has hecho los
-                deberes?
-              </p>
+              <p id="excusas-boss-text">{bossText}</p>
             </div>
           </div>
           <div
